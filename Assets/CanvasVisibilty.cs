084c4ee673a3316ad585730aa8be438fd9529d1c
ndasim <asim.dogan.namli@gmail.com>
2018-06-09 08:35:17 +0300

    09.06.2018

---
+++ Assets/CanvasVisibilty.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasVisibilty : MonoBehaviour {
+
+	BoxCollider collider;
+	Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+		collider = gameObject.AddComponent<BoxCollider>() as BoxCollider;
+		canvas = GetComponent<Canvas> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		canvas.enabled = collider.enabled;
+	}
+}
